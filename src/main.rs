363f49b750fdceb823bdacfae31e7dc6d7932a4c
jeff <jeffersonparil@gmail.com>
2021-08-22 14:05:03 +1000

    learning rust

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,13 @@
+mod structs_traits_methods;
+
 fn main() {
     println!("FASTX_STATS");
     println!("DNA sequence statistics");
+    // test Fasta
+    let seq1 = structs_traits_methods::Fasta {
+        name: String::from("> chr1:123-213:blah-blah"),
+        sequence: String::from("ATCGTAGCTATCATTAGCTAGCTAGCTGACTCATCTAGCT")
+    };
+    println!("Sequence: {}", seq1.name);
+    println!("{}", seq1.sequence);
 }
---
+++ src/read_fastx.rs
@@ -0,0 +1,5 @@
+// Read sequence data in fasta and fastq formats
+
+fn read_fasta() {
+    return(0)
+}
